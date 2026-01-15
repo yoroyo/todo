be5b6a3020378a121fe4aaee1f47ec232ed7ca05
yoshi <user@gmail.com>
2026-01-16 05:25:03 +0900

    todoサンプル

--- index.js
+++ index.js
@@ -1,4 +1,78 @@
 'use strict';
 
-module.exports = {
+// { name: タスクの文字列, state: 完了しているかどうかの真偽値 }
+const tasks = new Array();
+
+/**
+ * TODOを追加する
+ * @param {string} task
+ */
+function todo(task) {
+  tasks.push({ name: task, state: false });
 }
+
+/**
+* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了したかを返す
+* @param {object} taskAndIsDonePair
+* @return {boolean} 完了したかどうか
+*/
+function isDone(taskAndIsDonePair) {
+  return taskAndIsDonePair.state;
+}
+
+/**
+* タスクと完了したかどうかが含まれるオブジェクトを受け取り、完了していないかを返す
+* @param {object} taskAndIsDonePair
+* @return {boolean} 完了していないかどうか
+*/
+function isNotDone(taskAndIsDonePair) {
+  return !isDone(taskAndIsDonePair);
+}
+
+/**
+ * TODO一覧の配列を取得する
+ * @return {array}
+ */
+function list() {
+  return tasks.filter(isNotDone).map(t => t.name);
+}
+
+/**
+ * TODOを完了状態にする
+ * @param {string} task
+ */
+function done(task) {
+  const indexFound = tasks.findIndex(t => t.name === task);
+  if (indexFound !== -1) {
+    tasks[indexFound].state = true;
+  }
+}
+
+/**
+ * 完了済みのタスクの一覧の配列を取得する
+ * @return {array}
+ */
+function donelist() {
+  return tasks.filter(isDone).map(t => t.name);
+}
+
+/**
+ * 項目を削除する
+ * @param {string} task
+ */
+function del(task) {
+  const indexFound = tasks.findIndex(t => t.name === task);
+  if (indexFound !== -1) {
+    tasks.splice(indexFound, 1);
+  }
+}
+
+module.exports = {
+  todo,
+  list,
+  done,
+  donelist,
+  del
+};
+
+
